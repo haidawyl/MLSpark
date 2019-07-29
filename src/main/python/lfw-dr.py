aba32ac5a02fa250c002049f2f38ad25f7a4785d
haidawyl <haidawyl@163.com>
2019-07-29 22:07:56 +0800

    Update lfw-dr.py

--- src/main/python/lfw-dr.py
+++ src/main/python/lfw-dr.py
@@ -26,3 +26,12 @@ if __name__ == "__main__":
 
     pcs = np.loadtxt("D:/work/git/github/MLSpark/src/main/resources/data/lfw/pc.csv", delimiter=",")
     print(pcs.shape)
+
+    # 读取奇异值对应的向量S，为每个k绘制对应的奇异值图
+    s = np.loadtxt("D:/work/git/github/MLSpark/src/main/resources/data/lfw/s.csv", delimiter=",")
+    print(s.shape)
+    plt.plot(s)
+    plt.plot(np.cumsum(s))
+    plt.yscale('log')
+    # 在绘制的图中可以看到在k的某个区间之后（本例中大概是100），图形基本变平。
+    # 这表明多大的奇异值（或者主成分）的k值可以足够解释原始数据的变化。
